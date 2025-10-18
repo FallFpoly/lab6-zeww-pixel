3cbe42215777ccbb472cc41dbfd6da1d682082ec
ACER <ACER@172.16.105.115>
2025-10-18 08:50:05 +0700

    Nghia nop lab5

--- Lab6/src/main/java/com/mycompany/lab6/bai1.java
+++ Lab6/src/main/java/com/mycompany/lab6/bai1.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.lab6;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ICT
@@ -11,6 +13,32 @@ package com.mycompany.lab6;
 public class bai1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Nhap ho va ten: ");
+        String hoVaTen = sc.nextLine().trim();
+
+        
+        int firstSpace = hoVaTen.indexOf(" ");
+        int lastSpace = hoVaTen.lastIndexOf(" ");
+
+        
+        String ho = hoVaTen.substring(0, firstSpace).toUpperCase();
+
+        
+        String ten = hoVaTen.substring(lastSpace + 1).toUpperCase();
+
+        
+        String tenDem = "";
+        if (firstSpace != lastSpace) {
+            tenDem = hoVaTen.substring(firstSpace + 1, lastSpace);
+        }
+
+        
+        System.out.println("Ten: " + ten);
+        System.out.println("Ho: " + ho);
+        System.out.println("Ten dem: " + tenDem);
+
+        sc.close();
     }
 }
--- Lab6/src/main/java/com/mycompany/lab6/bai2.java
+++ Lab6/src/main/java/com/mycompany/lab6/bai2.java
@@ -4,6 +4,9 @@
 
 package com.mycompany.lab6;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author ICT
@@ -11,6 +14,46 @@ package com.mycompany.lab6;
 public class bai2 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+       Scanner sc = new Scanner(System.in);
+        ArrayList<SanPham> dsSanPham = new ArrayList<>();
+
+        
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Nhap thong tin san pham thu " + (i + 1) + ":");
+            SanPham sp = new SanPham();
+            sp.nhap(sc);
+            dsSanPham.add(sp);
+            System.out.println();
+        }
+
+        
+        System.out.println("Danh sach san pham hang Nokia:");
+        for (SanPham sp : dsSanPham) {
+            if (sp.hang.equalsIgnoreCase("Nokia")) {
+                sp.xuat();
+            }
+        }
+
+        sc.close();
+    }
+}
+class SanPham {
+    String tenSp;
+    double donGia;
+    String hang;
+
+    public void nhap(Scanner sc) {
+        System.out.print("Nhap ten san pham: ");
+        tenSp = sc.nextLine();
+
+        System.out.print("Nhap don gia: ");
+        donGia = Double.parseDouble(sc.nextLine());
+
+        System.out.print("Nhap hang san xuat: ");
+        hang = sc.nextLine();
+    }
+
+    public void xuat() {
+        System.out.println("Ten SP: " + tenSp + ", Gia: " + donGia + ", Hang: " + hang);
     }
 }
